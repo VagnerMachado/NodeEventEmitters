58c5ca84cb2b3319b9c437d291c9d06d4522168d
Vagner Machado <VagnerMachado84@hotmail.com>
2020-03-03 21:34:04 -0500

    Added comments to index.js

--- 05-morse-decoder/index.js
+++ 05-morse-decoder/index.js
@@ -2,9 +2,15 @@
 =======================
 05 - Morse Decoder - index.js
 =======================
-Student ID:
-Comment (Required):
-
+Student ID: 23651127
+Comment (Required): This class listens to events emitted form the MorseEmitter class.
+For that, it instantiates a MorseEmitter object and turns on listers for:
+	1 - Event ".": adds a dot character to morse_character.
+	2 - Event "-": adds a dash character to morse_character.
+	3 - Event "break": Indicates the end of a character encoding. 
+		3.1 - It prints the character onto console.
+		3.2 - Resets morse_character to ""
+	
 =======================
 */
 const MorseEmitter = require("./modules/MorseEmitter");
@@ -17,16 +23,20 @@ let morse_character = "";
 let place = 0;
 
 //no spec is sent by emitter, kept here as 'tradition' !
+
+//listens for "." event and adds dot to morse_character
 morse_emitter.on(".", (spec) => 
 {
 	morse_character += ".";
 });
 
+//listens for "-" event and adds a dash to morse_character
 morse_emitter.on("-", (spec) => 
 {
 	morse_character += "-";
 });
 
+//listens to a "break" event and prints a character to console. Resets the morse_character variable
 morse_emitter.on("break", (spec) => 
 {
 	if(place === 0)
@@ -37,4 +47,4 @@ morse_emitter.on("break", (spec) =>
 });
 
 
-//morse_emitter.start();
+morse_emitter.start();
